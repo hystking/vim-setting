4f2e0404e0edd49af5bce6c41afff47608cf5d80
Nakayama Yuhei <nakayama-yuhei@kayac.com>
2016-05-13 16:16:23 +0900

    move the update script

--- bundle/mine/dict/update.sh
+++
@@ -1,5 +0,0 @@
-curl https://raw.githubusercontent.com/asins/vim-dict/master/dict/css.dic > css.dict
-curl https://raw.githubusercontent.com/asins/vim-dict/master/dict/javascript.dic > javascript.dict
-curl https://raw.githubusercontent.com/asins/vim-dict/master/dict/node.dic > node.dict
-curl https://raw.githubusercontent.com/asins/vim-dict/master/dict/ruby.dic > ruby.dict
-curl https://raw.githubusercontent.com/asins/vim-dict/master/dict/php.dic > php.dict
---
+++ update.sh
@@ -0,0 +1,7 @@
+DICT_DIR=bundle/mine/dict
+
+curl https://raw.githubusercontent.com/asins/vim-dict/master/dict/css.dic > $DICT_DIR/css.dict
+curl https://raw.githubusercontent.com/asins/vim-dict/master/dict/javascript.dic > $DICT_DIR/javascript.dict
+curl https://raw.githubusercontent.com/asins/vim-dict/master/dict/node.dic > $DICT_DIR/node.dict
+curl https://raw.githubusercontent.com/asins/vim-dict/master/dict/ruby.dic > $DICT_DIR/ruby.dict
+curl https://raw.githubusercontent.com/asins/vim-dict/master/dict/php.dic > $DICT_DIR/php.dict
